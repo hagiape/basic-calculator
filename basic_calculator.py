6c5946fb1c507d0c86023b601a261939056b6bc9
hagiape <g_chrisf@protonmail.com>
2023-05-20 20:19:13 +0800

    Create __init__

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,6 +1,10 @@
 class BasicCalculator:
     # class variables
     result = 0
+    # constructor
+    def __init__(self, first_number, second_number):
+        self.first_number = first_number
+        self.second_number = second_number
     # calculator functions
     # add
     # subtract
