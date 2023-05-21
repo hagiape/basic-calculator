b44a07899a57104e881734b389af2deebc4cf6fd
hagiape <g_chrisf@protonmail.com>
2023-05-21 16:55:42 +0800

    Add conditionals for int and float conversions
    These are for the unneeded float conversion when performing operations.

--- basic_calculator.py
+++ basic_calculator.py
@@ -3,8 +3,15 @@ class BasicCalculator:
     result = 0
     # constructor
     def __init__(self, first_number, second_number, is_scientific = False, brand = 'Brand X'):
-        self.first_number = first_number
-        self.second_number = second_number
+        # this is to prevent unnecessary conversion of input to float
+        # when performing basic artithmetic operations. the result will 
+        # always be an integer except when performing simple division
+        if '.' in first_number or second_number:
+            self.first_number = float(first_number)
+            self.second_number = float(second_number)
+        else:
+            self.first_number = int(first_number)
+            self.second_number = int(second_number)
         self.is_scientific = is_scientific
         self.brand = brand
     # calculator functions
