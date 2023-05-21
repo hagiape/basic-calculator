21be7e9455ef0140aad54f45c149433bfd74c6f2
hagiape <g_chrisf@protonmail.com>
2023-05-22 00:48:04 +0800

    Add calculate method to optimize code
    The code has too many redundancies so an optimized code is implemented.

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,11 +1,24 @@
 class BasicCalculator:
     result = 0
 
-    def __init__(self, first_number, second_number, is_scientific = False, brand = 'Brand X'):
+    def __init__(self, first_number, operator, second_number, is_scientific = False, brand = 'Brand X'):
         self.first_number = float(first_number)
+        self.operator = operator
         self.second_number = float(second_number)
         self.is_scientific = is_scientific
         self.brand = brand
+    
+    def calculate(self, operator):
+        try:
+            if operator == '+':
+                self.result = self.first_number + self.second_number
+
+            if self.result.is_integer():
+                self.result = int(self.result)
+
+            print(self.result)
+        except ValueError:
+            print('There is an error. Please initialize the calculator properly.')
 
     def add_numbers(self):
         try:
@@ -49,6 +62,8 @@ class BasicCalculator:
         if self.is_scientific:
             try:
                 self.result = self.first_number ** self.second_number
+                if self.result.is_integer():
+                    self.result = int(self.result)
                 print(self.result)
             except:
                 print('There is an error. Please initialize the object properly.')
@@ -59,6 +74,8 @@ class BasicCalculator:
         if self.is_scientific:
             try:
                 self.result = self.first_number ** (1/self.second_number)
+                if self.result.is_integer():
+                    self.result = int(self.result)
                 print(self.result)
             except:
                 print('There is an error. Please initialize the object properly.')
