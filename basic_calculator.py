9de2cc9d68b458253182defad5afcc5a71e40de5
hagiape <g_chrisf@protonmail.com>
2023-05-21 11:59:34 +0800

    Add is_scientific and brand parameter
    Scientific calcualtor can perform more functions. Brand can now be stated.

--- basic_calculator.py
+++ basic_calculator.py
@@ -2,9 +2,11 @@ class BasicCalculator:
     # class variables
     result = 0
     # constructor
-    def __init__(self, first_number, second_number):
+    def __init__(self, first_number, second_number, is_scientific = False, brand = 'Brand X'):
         self.first_number = first_number
         self.second_number = second_number
+        self.is_scientific = is_scientific
+        self.brand = brand
     # calculator functions
     # add
     def add_numbers(self):
