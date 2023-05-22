a6bfa25b97f1453ff80276b8f65dd5387300370d
hagiape <g_chrisf@protonmail.com>
2023-05-22 17:04:03 +0800

    Remove redundant code
    Redundancies were removed since code were optimized in calculate()

--- basic_calculator.py
+++ basic_calculator.py
@@ -9,6 +9,9 @@ class BasicCalculator:
     
     def calculate(self):
         try:
+            # the conditionals below are additional methods and only applicable
+            # for the scientific calculator objects. 
+            # if is_scientific == False, the method wouldn't run
             if self.is_scientific:
                 if self.operator == '^':
                     BasicCalculator.result = self.first_number ** self.second_number
@@ -32,45 +35,7 @@ class BasicCalculator:
                     print('This calculator cannot perform that operation.')
         except ValueError:
             print('There is an error. Please initialize the calculator properly.')
-    
-    # the methods below are additional methods and only applicable
-    # for the scientific calculator objects. 
-    # if is_scientific == False, the method wouldn't run
-    def exponentiate(self):
-        if self.is_scientific:
-            try:
-                self.result = self.first_number ** self.second_number
-                if self.result.is_integer():
-                    self.result = int(self.result)
-                print(self.result)
-            except:
-                print('There is an error. Please initialize the object properly.')
-        else:
-            print('This calculator can only perform simple arithmetic operations!')
-            
-    def nth_root(self):
-        if self.is_scientific:
-            try:
-                self.result = self.first_number ** (1/self.second_number)
-                if self.result.is_integer():
-                    self.result = int(self.result)
-                print(self.result)
-            except:
-                print('There is an error. Please initialize the object properly.')
-        else:
-            print('This calculator can only perform simple arithmetic operations!')
 
-    def notation(self):
-        if self.is_scientific:
-            try:
-                self.result = self.first_number * (10 ** self.second_number)
-                if self.result.is_integer():
-                    self.result = int(self.result)
-                print(self.result)
-            except:
-                print('There is an error. Please initialize the object properly.')
-        else:
-            print('This calculator can only perform simple arithmetic operations!')
     # this is similar to the "Ans" button in some calculators
     # which will print the answer of the last operation performed 
     def last_answer(self):
