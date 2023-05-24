3fcb5975a06ad28abce0ccece885d2dbabb21d71
hagiape <g_chrisf@protonmail.com>
2023-05-24 15:35:18 +0800

    Update basic_calculator.py
    The border() is now inside BasicCalculator (BasicCalculator.border()).

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,19 +1,20 @@
-# the code was modified, originally from Stack Overflow:
-# https://stackoverflow.com/a/20757225
-# function that creates text border based on the output
-def border(string):
-    text = string.splitlines()
-    max_length = max(len(s) for s in text)
-    column_width = max_length + 2
-    print('+' + ('-' * column_width) + '+')
-    for s in text:
-        print('| %-*.*s |' % (max_length, max_length, ''))
-        print('| %-*.*s |' % (max_length, max_length, s))
-        print('| %-*.*s |' % (max_length, max_length, ''))
-    print('+' + ('-' * column_width) + '+')
-
 class BasicCalculator:
     answer = 0
+
+    # this code was modified, originally from Stack Overflow:
+    # https://stackoverflow.com/a/20757225
+    # function that creates text border
+    def border(string):
+        text = string.splitlines()
+        max_length = max(len(s) for s in text)
+        column_width = max_length + 2
+        print('+' + ('-' * column_width) + '+')
+        for s in text:
+            print('| %-*.*s |' % (max_length, max_length, ''))
+            print('| %-*.*s |' % (max_length, max_length, s))
+            print('| %-*.*s |' % (max_length, max_length, ''))
+        print('+' + ('-' * column_width) + '+')
+
     def __init__(self, first_number, operator, second_number, is_scientific = False, brand = 'Brand X'):
         self.first_number = float(first_number)
         self.operator = operator
@@ -38,30 +39,30 @@ class BasicCalculator:
 
                 if BasicCalculator.answer.is_integer():
                     BasicCalculator.answer = int(BasicCalculator.answer)
-                border('The answer is: ' + str(BasicCalculator.answer))
+                BasicCalculator.border('The answer is: ' + str(BasicCalculator.answer))
             else:
                 if self.operator in ['+', '-', '*', '/']:
                     BasicCalculator.answer = eval(f"self.first_number {self.operator} self.second_number")
                     if BasicCalculator.answer.is_integer():
                         BasicCalculator.answer = int(BasicCalculator.answer)
-                    border('The answer is: ' + str(BasicCalculator.answer))
+                    BasicCalculator.border('The answer is: ' + str(BasicCalculator.answer))
                 else:
-                    border('This calculator cannot perform that operation.')
+                    BasicCalculator.border('This calculator cannot perform that operation.')
         except:
-            border('There is an error. Please initialize the calculator properly.')
+            BasicCalculator.border('There is an error. Please initialize the calculator properly.')
     
     def show_info(self):
         if self.brand == 'Brand X':
-            print('This is a generic brand of calculator.')
+            BasicCalculator.border('This is a generic brand of calculator.')
         else:
-            print('This is a ' + self.brand + ' calculator.')
+            BasicCalculator.border('This is a ' + self.brand + ' calculator.')
 
         if self.is_scientific:
-            print('This is a scientific calculator.')
+            BasicCalculator.border('This is a scientific calculator.')
         else:
-            print('This is a regular calculator.')
+            BasicCalculator.border('This is a regular calculator.')
 
     # this is similar to the "Ans" button in some calculators
     # which will print the answer of the last operation performed 
     def last_answer(self):
-        border('The last answer is: ' + str(BasicCalculator.answer))
+        BasicCalculator.border('The last answer is: ' + str(BasicCalculator.answer))
