6378c9da3796195b2a66fa56b89d8910711082f3
hagiape <g_chrisf@protonmail.com>
2023-06-03 16:41:32 +0800

    Update basic_calculator.py
    Integer conversion was added to prevent unnecessary float conversion.

--- basic_calculator.py
+++ basic_calculator.py
@@ -14,23 +14,31 @@ class BasicCalculator:
             print('| %-*.*s |' % (max_length, max_length, ''))
         print('+' + ('-' * column_width) + '+')
 
+    def integer_conversion():
+        if BasicCalculator.answer.is_integer:
+            BasicCalculator.answer = int(BasicCalculator.answer)
+
     def __init__(self, brand = 'Brand X'):
         self.brand = brand
 
     def add(self, first_number, second_number):
-        BasicCalculator.answer = first_number + second_number
+        BasicCalculator.answer = float(first_number) + float(second_number)
+        BasicCalculator.integer_conversion()
         return BasicCalculator.answer
     
     def subtract(self, first_number, second_number):
-        BasicCalculator.answer = first_number + second_number
+        BasicCalculator.answer = float(first_number) + float(second_number)
+        BasicCalculator.integer_conversion()
         return BasicCalculator.answer
     
     def multiply(self, first_number, second_number):
-        BasicCalculator.answer = first_number, second_number
+        BasicCalculator.answer = float(first_number) * float(second_number)
+        BasicCalculator.integer_conversion()
         return BasicCalculator.answer
     
     def divide(self, first_number, second_number):
-        BasicCalculator.answer = first_number, second_number
+        BasicCalculator.answer = float(first_number) / float(second_number)
+        BasicCalculator.integer_conversion()
         return BasicCalculator.answer
 
     # this is similar to the "Ans" button in some calculators
