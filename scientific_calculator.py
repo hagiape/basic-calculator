9008cb5b3d494da50c26ea3cf57f4a178cfd83c5
hagiape <g_chrisf@protonmail.com>
2023-06-03 16:52:50 +0800

    Update scientific_calculator.py
    Integer conversion was added to prevent unnecessary float conversion.

--- scientific_calculator.py
+++ scientific_calculator.py
@@ -3,10 +3,13 @@ from basic_calculator import BasicCalculator
 class ScientificCalculator(BasicCalculator):
     def exponentiate(self, first_number, second_number):
         ScientificCalculator.answer = first_number ** second_number
+        ScientificCalculator.integer_conversion()
         return ScientificCalculator.answer
     def root(self, first_number, second_number):
         ScientificCalculator.answer = first_number ** (1/second_number)
+        ScientificCalculator.integer_conversion()
         return ScientificCalculator.answer
     def notation(self, first_number, second_number):
         ScientificCalculator.answer = first_number * (10 ** second_number)
+        ScientificCalculator.integer_conversion()
         return ScientificCalculator.answer
