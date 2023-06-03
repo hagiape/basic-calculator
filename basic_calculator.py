645e51f4e44c690a9f7dd578100259cf411d88a3
hagiape <g_chrisf@protonmail.com>
2023-06-03 16:22:02 +0800

    Update basic_calculator.py
    The old code was deleted since it won't used anymore.

--- basic_calculator.py
+++ basic_calculator.py
@@ -32,47 +32,7 @@ class BasicCalculator:
     def divide(self, first_number, second_number):
         BasicCalculator.answer = first_number, second_number
         return BasicCalculator.answer
-    
-    # def calculate(self):
-    #     try:
-    #         # the conditionals below are additional methods and only applicable
-    #         # for the scientific calculator objects. 
-    #         # if is_scientific == False, the method wouldn't run
-    #         if self.is_scientific:
-    #             if self.operator == '^':
-    #                 BasicCalculator.answer = self.first_number ** self.second_number
-    #             elif self.operator == 'root':
-    #                 BasicCalculator.answer = self.first_number ** (1/self.second_number)
-    #             elif self.operator == 'notation':
-    #                 BasicCalculator.answer = self.first_number * (10 ** self.second_number)
-    #             elif self.operator in ['+', '-', '*', '/']:
-    #                 BasicCalculator.answer = eval(f"self.first_number {self.operator} self.second_number")
 
-    #             if BasicCalculator.answer.is_integer():
-    #                 BasicCalculator.answer = int(BasicCalculator.answer)
-    #             BasicCalculator.border('The answer is: ' + str(BasicCalculator.answer))
-    #         else:
-    #             if self.operator in ['+', '-', '*', '/']:
-    #                 BasicCalculator.answer = eval(f"self.first_number {self.operator} self.second_number")
-    #                 if BasicCalculator.answer.is_integer():
-    #                     BasicCalculator.answer = int(BasicCalculator.answer)
-    #                 BasicCalculator.border('The answer is: ' + str(BasicCalculator.answer))
-    #             else:
-    #                 BasicCalculator.border('This calculator cannot perform that operation.')
-    #     except:
-    #         BasicCalculator.border('There is an error. Please initialize the calculator properly.')
-    
-    # def show_info(self):
-    #     if self.brand == 'Brand X':
-    #         BasicCalculator.border('This is a generic brand of calculator.')
-    #     else:
-    #         BasicCalculator.border('This is a ' + self.brand + ' calculator.')
-
-    #     if self.is_scientific:
-    #         BasicCalculator.border('This is a scientific calculator.')
-    #     else:
-    #         BasicCalculator.border('This is a regular calculator.')
-    
     # this is similar to the "Ans" button in some calculators
     # which will print the answer of the last operation performed 
     def last_answer(self):
