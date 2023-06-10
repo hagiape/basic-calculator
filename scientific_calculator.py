56c927b36a136d215fbe0dd811c672f779930512
hagiape <g_chrisf@protonmail.com>
2023-06-10 19:58:39 +0800

    Update basic_calculator.py
    The code used self keyword whenever class variable answer is called.

--- scientific_calculator.py
+++ scientific_calculator.py
@@ -1,23 +1,20 @@
 from basic_calculator import BasicCalculator
 
 class ScientificCalculator(BasicCalculator):
-    def integer_conversion(self):
-        if ScientificCalculator.answer.is_integer:
-            ScientificCalculator.answer = int(ScientificCalculator.answer)
     def exponentiate(self, first_number, second_number):
-        ScientificCalculator.answer = float(first_number ** second_number)
-        ScientificCalculator.integer_conversion(self)
-        return ScientificCalculator.answer
+        self.answer = float(first_number ** second_number)
+        self.integer_conversion()
+        return self.answer
     def root(self, first_number, second_number):
-        ScientificCalculator.answer = float(first_number ** 1/second_number)
-        ScientificCalculator.integer_conversion(self)
-        return ScientificCalculator.answer
+        self.answer = float(first_number ** 1/second_number)
+        self.integer_conversion()
+        return self.answer
     def notation(self, first_number, second_number):
-        ScientificCalculator.answer = float(first_number * 10 ** second_number)
-        ScientificCalculator.integer_conversion(self)
-        return ScientificCalculator.answer
+        self.answer = float(first_number * 10 ** second_number)
+        self.integer_conversion()
+        return self.answer
     def show_info(self):
         if self.brand == 'Brand X':
-            ScientificCalculator.border('This is a generic brand of a scientific calculator.')
+            self.border('This is a generic brand of a scientific calculator.')
         else:
-            ScientificCalculator.border('This is a ' + self.brand + ' scientific calculator.')
+            self.border('This is a ' + self.brand + ' scientific calculator.')
